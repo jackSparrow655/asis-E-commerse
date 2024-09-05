f0e7c95b14793a966db581ea8eecd6c352df1fc1
Arijit655 <arijitbarik655@gmail.com>
2024-09-05 17:12:39 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+require('dotenv').config()
+const connectDB = require('./config/db')
+ const router = require('./routes')
+
+
+const app = express()
+app.use(cors({
+    origin : process.env.FRONTEND_URL,
+    credentials : true
+}))
+app.use(express.json())
+app.use(cookieParser())
+app.get("/",(req,res)=>{
+    res.send("<h1>Ecommerce_me</h1>");
+  });
+
+app.use("/api",router)
+
+const PORT = 5000 || process.env.PORT
+
+
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log("connnect to DB")
+        console.log("Server is running on "+PORT)
+    })
+})
